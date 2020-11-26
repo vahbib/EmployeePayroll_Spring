d27c307722cfadbf61876acad1b543d7ab782f6f
Bibhav Singh <singhbibhav084@gmail.com>
2020-11-26 14:20:55 +0530

    Employee Payroll CRUD Operation

---
+++ src/test/java/com/spring/employeepayrollapp/EmployeePayrollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.employeepayrollapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
